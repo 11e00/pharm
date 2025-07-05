9bead54069b49e76e69e1297bd8f2e1a089c5635
Kazaction <ps4psnkikos@gmail.com>
2025-07-05 16:03:26 +0300

    Products by category

---
+++ src/app/categories/category/page.tsx
@@ -0,0 +1,31 @@
+import {createClient} from "@supabase/supabase-js";
+import CategoryList from "@/app/categories/catList";
+import ProdList from "@/app/categories/category/prodList";
+const supabaseUrl=process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey=process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+const supabase=createClient(supabaseUrl, supabaseKey);``
+
+export default async function productsCategories() {
+    let product_categories = [];
+
+    try{
+        let { data:product_category, error:apiError }=await supabase
+            .from('Drugs')
+            .select('*');
+        console.log(product_category);
+
+        if(apiError) throw apiError;
+        product_categories=product_category ?? [];
+    }catch(err){
+        //network error
+        console.error(err);
+    }
+
+
+    return (
+        <div className="categories">
+            <h1>Categories fueh</h1>
+            <ProdList product_categories={product_categories ?? []} />
+        </div>
+    );
+}
---
+++ src/app/categories/category/prodList.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+type Products = {
+    drug_id: string;
+    price: string;
+    name: string;
+    stock: string;
+    visible: string;
+    category_name: string;
+};
+
+export default function ProdList({ product_categories }: { product_categories: Products[] }) {
+
+    function LoadCatProd(prod: Products) {
+        console.log('Category selected:', prod);
+        if(prod.drug_id=='1'){
+
+        }
+    }
+
+    return (
+        <div className="categories">
+            {product_categories.map((prod) => (
+                <div key={prod.drug_id} onClick={() => LoadCatProd(prod)} style={{ cursor: 'pointer'}}>
+                    [ {prod.name} ]
+                </div>
+            ))}
+        </div>
+    );
+}
