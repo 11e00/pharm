7732c0e55612f74b0fe08b5257094a4eca4ddaab
11e00 <leontios.lion7@gmail.com>
2025-07-18 00:14:58 +0300

    categories page ui change

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -22,35 +22,26 @@ export default async function Categories() {
   
   return (
     <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-2 lg:max-w-7xl">
-            {categories.map((category:any) => (
-                <div key={category.category_id} className="m-10 relative overflow-hidden rounded-lg lg:h-96">
-                    <div className="absolute inset-0">
-                        <img
-                        alt=""
-                        src="https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-featured-collection.jpg"
-                        className="size-full object-cover"
-                        />
-                    </div>
-                    <div aria-hidden="true" className="relative h-96 w-full lg:hidden" />
-                    <div aria-hidden="true" className="relative h-32 w-full lg:hidden" />
-                    <div className="absolute inset-x-0 bottom-0 rounded-br-lg rounded-bl-lg bg-black/75 p-6 backdrop-blur-sm backdrop-filter sm:flex sm:items-center sm:justify-between lg:inset-x-auto lg:inset-y-0 lg:w-96 lg:flex-col lg:items-start lg:rounded-tl-lg lg:rounded-br-none">
-                        <div>
-                        <h2 className="text-xl font-bold text-white">{category.category_name}</h2>
-                        <p className="mt-1 text-sm text-gray-300">
-                            Upgrade your desk with objects that keep you organized and clear-minded.
-                        </p>
-                        </div>
-                        <Link
-                        href={"/categories/"+category.category_name}
-                        className="mt-6 flex shrink-0 items-center justify-center rounded-md border border-white/25 px-4 py-3 text-base font-medium text-white hover:bg-white/10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full"
-                        >
-                        View the collection
-                        </Link>
-                    </div>
-                </div>
-            ))}
+        <div className="mx-auto max-w-xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Shop by Collection</h2>
+        <p className="mt-4 text-base text-gray-500">
+          Each season, we collaborate with world-class designers to create a collection inspired by the natural world.
+        </p>
+
+        <div className="mt-10 space-y-12 lg:grid lg:grid-cols-3 lg:space-y-0 lg:gap-x-8">
+          {categories.map((category:any) => (
+            <Link key={category.category_name} href={"/categories/"+category.category_name} className="group block">
+              <img
+                alt="item"
+                src="https://www.premier-health.co.uk/images/pharmacy/Own_Label_Market_Pharmacy_Range.png"
+                className="aspect-3/2 w-full rounded-lg object-cover group-hover:opacity-75 lg:aspect-5/6"
+              />
+              <h3 className="mt-4 text-base font-semibold text-gray-900">{category.category_name}</h3>
+              <p className="mt-2 text-sm text-gray-500">Item Description</p>
+            </Link>
+          ))}
         </div>
+      </div>
     </div>
 
   );
