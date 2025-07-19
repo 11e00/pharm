0cad61306c7426c786abfd56a1b7c1b15ca234a2
11e00 <leontios.lion7@gmail.com>
2025-07-19 03:55:51 +0300

    default 404 page change + implementation on false dynamic routes

--- src/app/categories/[category]/[product]/page.tsx
+++ src/app/categories/[category]/[product]/page.tsx
@@ -1,13 +1,34 @@
 import {createClient} from "@supabase/supabase-js";
+import { notFound } from 'next/navigation';
 import LoadProduct from "./clientFunctions";
+
 const supabaseUrl=process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey=process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 const supabase=createClient(supabaseUrl, supabaseKey);
 
 export default async function ProductPage({params}:{params:{product:string}}) {
     //TODO line below fix await error
-    const productID=params.product;
-        let product = [];
+    const urlProductID=params.product;
+    let productID:any = [];
+    let product = [];
+    
+
+    try {
+        const { data: productIDData, error: apiError } = await supabase
+            .from('Drug')
+            .select('drug_id')
+            .eq('drug_id', urlProductID);
+
+        if (apiError) throw apiError;
+        productID=productIDData[0]?.drug_id ?? [];
+
+    } catch (err) {
+        //network error
+        console.error(err);
+    }
+
+    if(urlProductID!=productID)
+        notFound();
 
     try{
         let { data:productArr, error:apiError }=await supabase
--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import LoadProducts from "./clientFunctions";
+import { redirect } from 'next/navigation';
+import { notFound } from 'next/navigation';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
@@ -7,15 +9,33 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 //TODO Make "All-items" return all items
 export default async function CategoryPage({ params }: { params: { category: string } }) {
-    const categoryName = params.category;        
+    const urlCategoryName = params.category;
+    let categoryName:any = [];
     let categoryID:any = [];
     let products:any=[];
+
+    try {
+        const { data: categoryNameData, error: apiError } = await supabase
+            .from('Category')
+            .select('category_name')
+            .eq('category_name', urlCategoryName);
+
+        if (apiError) throw apiError;
+        categoryName=categoryNameData[0]?.category_name ?? [];
+
+    } catch (err) {
+        //network error
+        console.error(err);
+    }
+
+    if(urlCategoryName!=categoryName)
+        notFound();
 
     try {
         const { data: categoryIDdata, error: apiError } = await supabase
             .from('Category')
             .select('category_id')
-            .eq('category_name', categoryName);
+            .eq('category_name', urlCategoryName);
 
         if (apiError) throw apiError;
         categoryID=categoryIDdata[0].category_id ?? [];
@@ -46,7 +66,7 @@ export default async function CategoryPage({ params }: { params: { category: str
                 const { data: productArr, error: apiError } = await supabase
                     .from('Drug')
                     .select('drug_id, name, price, stock, visible, Category!inner(category_name, category_id)')
-                    .eq('Category.category_name', categoryName);
+                    .eq('category_name', urlCategoryName);
 
                 if (apiError) throw apiError;
                 products=productArr ?? [];
@@ -57,6 +77,6 @@ export default async function CategoryPage({ params }: { params: { category: str
             break;
     }
     return (
-        <LoadProducts currentCategory={categoryName} products={products} />
+        <LoadProducts currentCategory={urlCategoryName} products={products} />
     );
 }
